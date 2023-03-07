b9c1b07bf68449558110ba684efde73955d7a678
Ksenia91Dumina <100465785+Ksenia91Dumina@users.noreply.github.com>
2023-03-07 20:41:42 +0300

    add-main-service

---
+++ main-service/src/main/java/ru/practicum/comment/dto/CommentDtoNew.java
@@ -0,0 +1,17 @@
+package ru.practicum.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CommentDtoNew {
+    @NotBlank
+    private String text;
+}
---
+++ main-service/src/main/java/ru/practicum/comment/model/Comment.java
@@ -0,0 +1,31 @@
+package ru.practicum.comment.model;
+
+import lombok.*;
+import ru.practicum.event.model.Event;
+import ru.practicum.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String text;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+    @Column
+    private LocalDateTime created;
+}
